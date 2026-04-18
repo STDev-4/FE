516145e26c2f5c70124f75eb90dd97b1d7302b28
hyeonkangkimm <kkhk75@gmail.com>
2026-04-18 18:03:32 +0900

    Refactor: 로딩 바 수정

--- src/app/components/WelcomePage.tsx
+++ src/app/components/WelcomePage.tsx
@@ -155,7 +155,7 @@ export default function WelcomePage() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.4 }}
-          className="w-full pb-6 flex flex-col gap-3"
+          className="w-full pb-0 flex flex-col gap-3"
         >
           <button
             onClick={() => navigate("/login")}
